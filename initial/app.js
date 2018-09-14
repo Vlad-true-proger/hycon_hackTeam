971a5f2d6a125ba2e9302b235897b18de61f9645
Ziyi <ziyi-26@hotmail.com>
2018-09-14 23:04:26 +0200

    HTML update

--- initial/app.js
+++ initial/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cryptojs = require('./public/cryptojs');
 var usersRouter = require('./routes/users');
 
 var app = express();
